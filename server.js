c86dcc1174ddb59d054d16813bcf3da1d734868d
neilshah2000 <neil.shah@thomsonreuters.com>
2016-07-02 16:30:26 +0100

    server changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var server = express();
-<<<<<<< HEAD
+
 
 server.set('port', (process.env.PORT || 5000));
 
@@ -15,7 +15,4 @@ server.get('/', function(request, response) {
 server.listen(server.get('port'), function() {
   console.log('Node app is running on port', server.get('port'));
 });
-=======
-server.use(express.static(__dirname));
-server.listen(8888);
->>>>>>> 25de0b42d0addcbf369ba17bd6e803cd22f06d99
+
